bd2bc3e5387c43d6f2745b029a6abea88fc5f6ca
Lei Zongmin <leizongmin@gmail.com>
2011-09-10 13:57:04 +0800

    router

--- core/router.js
+++ core/router.js
@@ -18,10 +18,7 @@ router.limitMethods = ['get', 'post', 'delete', 'put', 'head'];
 /** 注册的处理函数 */
 router.handlers = {}
 for (var i in router.limitMethods)
-	router.handlers[i] = {}
-	
-/** 正则表达式缓存 */
-router.regexpCache = {}
+	router.handlers[router.limitMethods[i]] = []
 
 
 /**
@@ -39,7 +36,7 @@ router.register = function (method, paths, handler) {
 		return false;
 	}
 	method = method.toLowerCase().trim();
-	if (!(method in router.limitMethods)) {
+	if (!(method in router.handlers)) {
 		debug('Method [' + method + '] not limited.');
 		return false;
 	}
@@ -49,12 +46,73 @@ router.register = function (method, paths, handler) {
 		return false;
 	}
 	
-	// 注册处理函数
-	router.handlers[method][paths] = handler;
+	/* 处理paths */
+	// 获取名称
+	var names = paths.match(/\/:[\w\d_$]+/g);
+	if (!names)
+		names = [];
+	names.forEach(function (v, i) {
+		names[i] = v.substr(2);
+	});
+	// 替换正则表达式
+	var paths = paths.replace(/\/:[\w\d_$]+/g, '(/[^/]+)');
+	paths = '^' + paths + '$';
+	var pathreg = new RegExp(paths);
 	
-	// 缓存正则表达式
-	router.regexpCache[paths]
+	// 注册处理函数
+	router.handlers[method].push({
+		path:		pathreg,	// RegExp实例
+		names:		names,		// 变量名称
+		handler:	handler		// 处理函数
+	});
+	
+	return true;
 }
 
 /**
- * 获取处理
+ * 获取处理函数
+ *
+ * @param {string} method 请求类型(CET|POST|DELETE|PUT|HEAD)
+ * @param {string} paths 请求路径
+ * @return {object} 返回格式：{handler:处理函数, value:解析处理的键值} 
+ */
+router.handler = function (method, paths) {
+	// 参数检查
+	if (typeof method != 'string' || typeof paths != 'string') {
+		debug('get handler fail: arguments data type error.');
+		return false;
+	}
+	method = method.toLowerCase().trim();
+	if (!(method in router.handlers)) {
+		debug('Method [' + method + '] not limited.');
+		return false;
+	}
+	paths = paths.trim();
+	if (paths == '') {
+		debug('Paths could not be empty.');
+		return false;
+	}
+	
+	// 测试符合条件的处理函数
+	var handlers = router.handlers[method];
+	for (i in handlers) {
+		var r = handlers[i];
+		var pv = r.path.exec(paths);
+		if (pv) {
+			debug('match ' + paths);	// debug(pv);
+			// 填充匹配的PATH值
+			var ret = {
+				handler: r.handler,
+				value:	{}
+			}
+			r.names.forEach(function (v, i) {
+				ret.value[v] = pv[i + 1].substr(1);
+			});
+			
+			return ret;
+		}
+	}
+	
+	// 没有符合条件的处理函数
+	return false;
+}
