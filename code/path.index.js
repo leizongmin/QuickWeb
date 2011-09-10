fad9d199d4557ba8ca06268b79091f06a67328da
leizongmin <leizongmin@gmail.com>
2011-09-10 23:52:17 +0800

    POST数据解析

--- code/path.index.js
+++ code/path.index.js
@@ -10,3 +10,12 @@ exports.get = function (server, request, response) {
 	response.setCookie('bb', 'mmqq', {maxAge: 600});
 	response.end('Hello, world! ');
 }
+
+exports.post = function (server, request, response) {
+	var html = '';
+	for (var i in request.post)
+		html += i + ' = ' + request.post[i] + '\n';
+	response.end(html);
+	
+	console.log(request.file);
+}
---
+++ plus/00.post.js
@@ -0,0 +1,29 @@
+/**
+ * 插件：POST数据解析
+ *
+ */
+ 
+var formidable = require('formidable');
+
+exports.init_request = function (web, request, debug) { 
+	request.addListener(function (req) {
+		
+		var method = req.method.toLowerCase();
+		if (method == 'post' || method == 'put') {
+			// 解析
+			var form = new formidable.IncomingForm();
+			form.parse(req.origin, function (err, fields, files) {
+				req.post = fields;
+				req.file = files;
+				
+				// 通知下一个监听器
+				req.next();
+			});
+		}
+		else {
+			// 通知下一个监听器
+			req.next();
+		}
+		
+	}, true);
+}
