9e6623c0966dfcf761f1ebadf84d76f5b753812c
Lei Zongmin <leizongmin@gmail.com>
2011-09-15 17:44:50 +0800

    演示程序start.js

--- start.js
+++ start.js
@@ -9,10 +9,10 @@
 var web = require('QuickWeb');
 
 // settings
-web.set('wwwroot', './www');			// file path (file_server plus)
-web.set('code_path', './code');			// code path (restful router plus)
-web.set('template_path', './tpl');		// template path (render plus)
-web.set('template_extname', 'html');	// template extname (render plus)
+web.set('wwwroot', './test/www');				// file path (file_server plus)
+web.set('code_path', './test/code');			// code path (restful router plus)
+web.set('template_path', './test/tpl');			// template path (render plus)
+web.set('template_extname', 'html');			// template extname (render plus)
 
 // load all plus
 web.loadPlus();
