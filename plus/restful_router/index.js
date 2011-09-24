ad530288a11020657d849d4c8684dc72e3e2fa36
Lei Zongmin <leizongmin@gmail.com>
2011-09-24 15:32:41 +0800

    自动更新code_path目录

--- plus/restful_router/index.js
+++ plus/restful_router/index.js
@@ -8,25 +8,17 @@ var web = require('../../core/web');
  
 var fs = require('fs');
 var path = require('path'); 
+
+/* 代码文件列表 */
+var code_file_array = [];
  
 exports.init_server = function (web, server) {
 	
-	/** 加载path模块 */
+	/** 加载路由处理程序 */
 	var code_path =  web.get('code_path');
 	var files = scanCodeFiles(code_path);
-	files.forEach(function (v) {
-		web.log('router', 'Load code file [' + v + ']', 'debug');
-		var m = require(v);
-		if (typeof m.paths != 'string')
-			return;
-		
-		// 注册处理程序
-		registerCodeFile(m);
-		
-		// 监视文件改动
-		watchCodeFile(v);
-	});
-	
+	loadCodeFiles(files);		// 载入代码
+	watchCodePath(code_path);	// 监视该目录的更新
 	
 	/** 注册监听器 */
 	server.addListener(function (svr, req, res) {
@@ -55,7 +47,7 @@ var scanCodeFiles = function (code_path) {
 	try {
 		var files = fs.readdirSync(code_path);
 		files.forEach(function (v) {
-			if (path.extname(v))
+			if (path.extname(v) == '.js')
 				ret.push(path.resolve(code_path, v));
 		});
 		return ret;
@@ -113,4 +105,87 @@ var watchCodeFile = function (filename) {
 			web.log('reload code file', err, 'error');
 		}
 	});
+	
+	// 将文件名加入到代码文件名列表
+	for (var i in code_file_array)
+		if (code_file_array[i] == filename)
+			return;
+	code_file_array.push(filename);
+}
+
+/**
+ * 载入一组程序文件，并注册
+ *
+ * @param {array} files 文件名数组
+ */
+var loadCodeFiles = function (files) {
+	files.forEach(function (v) {
+		web.log('router', 'Load code file [' + v + ']', 'debug');
+		try {
+			// 载入模块
+			var m = require(v);
+			if (typeof m.paths != 'string')
+				return;
+			
+			// 注册处理程序
+			registerCodeFile(m);
+			
+			// 监视文件改动
+			watchCodeFile(v);
+		}
+		catch (err) {
+			web.log('load code file', err, 'error');
+		}
+	});
+}
+
+/**
+ * 监视指定代码目录，如果有新文件，则自动载入
+ *
+ * @param {string} code_path 代码目录
+ */
+var watchCodePath = function (code_path) {
+	code_path = path.resolve(code_path);
+	fs.unwatchFile(code_path);
+	fs.watchFile(code_path, function () {
+		web.log('code path changed', code_path, 'info');
+		
+		// 重新扫描目录的文件
+		var files = scanCodeFiles(code_path);
+		
+		// 过滤，只保留新增的文件名
+		files = subArray(files, code_file_array);
+		
+		// 加载
+		loadCodeFiles(files);
+	});
+}
+
+/**
+ * 如果元素A在数组中，则删除它
+ *
+ * @param {array} arr 数组
+ * @param {string} v 元素
+ * @return {array}
+ */
+var delFromArray = function (arr, v) {
+	for (var i in arr)
+		if (arr[i] == v) {
+			arr.splice(i, 1);
+			break;
+		}
+	return arr;
+}
+
+/**
+ * 数组A - B
+ *
+ * @param {array} a 数组A
+ * @param {array} b 数组B
+ * @return {array}
+ */
+var subArray = function (a, b) {
+	for (var i in b)
+		a = delFromArray(a, b[i]);
+	return a;
 }
