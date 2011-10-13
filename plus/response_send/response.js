c6e6f13a03c37d389b3cdc7354984fe880adbc83
Lei Zongmin <leizongmin@gmail.com>
2011-10-13 18:49:06 +0800

    新增response.sendJSONIfAccepted()，如果客户端接受JSON格式数据，则发送JSON数据，否则，调用回调函数

--- plus/response_send/response.js
+++ plus/response_send/response.js
@@ -89,4 +89,24 @@ exports.init_response = function (web, response) {
 		this.writeHead(code, {'Content-type': 'text/html'});
 		this.end('<h3>' + msg + '</h3><hr><strong>QuickWeb ' + web.version + '</strong> &nbsp; ' + new Date().toUTCString());
 	}
+	
+	/**
+	 * 如果客户端接受JSON格式数据，则发送JSON数据，否则，调用回调函数
+	 *
+	 * @param {object} data 已JSON格式发送的数据
+	 * @param {function} callback 如果客户端不接受JSON数据时的回调函数
+	 */
+	response.ServerResponse.prototype.sendJSONIfAccepted = function (data, callback) {
+		var accept = this._link.request.headers['accept'];
+		// web.log('sendJSONIfAccepted', accept, 'debug');
+		if (typeof accept == 'string' && /application\/json/i.test(accept)) {
+			this.end(JSON.stringify(data));
+		}
+		else {
+			if (typeof callback == 'function')
+				callback();
+			else
+				web.log('sendJSONIfAccept', 'callback is not a function', 'error');
+		}
+	}
 }
