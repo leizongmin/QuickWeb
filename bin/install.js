1349612dc8e6549aa44b6b755a6b7cfa900207b7
leizongmin <leizongmin@gmail.com>
2011-12-27 10:50:38 +0800

    install

--- bin/install.js
+++ bin/install.js
@@ -10,20 +10,21 @@ var path = require('path');
 var os = require('os'); 
 var os_type = os.type();
 var exec = require('child_process').exec;
+var line = console.log;
 
 // 如果为Windows系统，则创建quickweb.cmd文件
 if (/Windows/ig.test(os_type)) {
 	try {
 		var filename = path.resolve(__dirname, 'quickweb');
 		var binname = 'C:\\Windows\\quickweb.cmd';
-		var cmdscript = 'node ' + filename + ' %1';
+		var cmdscript = 'node ' + filename + ' %*';
 		var err = fs.writeFileSync('quickweb.cmd', cmdscript);
 		err = fs.writeFileSync(binname, cmdscript);
 	}
 	catch (err) {
-		console.log(err.stack);
-		console.log('======================================================');
-		console.log('Please copy the file "quickweb.cmd" to Windows system directory.\nRun this command:\ncopy ' + filename + ' ' + binname);
+		line(err.stack);
+		line('======================================================');
+		line('Please copy the file "quickweb.cmd" to Windows system directory.\nRun this command:\ncopy ' + filename + ' ' + binname);
 	}
 }
 // 为Linux系统，将quickweb.js链接到/usr/bin目录
@@ -32,23 +33,25 @@ else {
 	var binname = '/usr/bin/quickweb';
 	var cmd1 = 'ln -s -f ' + filename + ' /usr/bin';
 	var cmd2 = 'chmod 777 ' + binname;
-	console.log(cmd1 + '\n' + cmd2);
+	line(cmd1 + '\n' + cmd2);
 	try {
 		exec(cmd1, function (error, stdout, stderr) {
 			if (error)
-				console.log(error.stack);
+				line(error.stack);
 			exec(cmd2, function (error, stdout, stderr) {
 				if (error) {
-					console.log(error.stack);
-					console.log('======================================================');
-					console.log('Please run this command to finish install QuickWeb:\n' + cmd1 + '\n' + cmd2);
+					line(error.stack);
+					line('======================================================');
+					line('Please run this command to finish install QuickWeb:\n' + cmd1 + '\n' + cmd2);
 				}
 			});
 		});
 	}
 	catch (err) {
-		console.log(err.stack);
-		console.log('======================================================');
-		console.log('Please run this command to finish install QuickWeb:\n' + cmd1 + '\n' + cmd2);
+		line(err.stack);
+		line('======================================================');
+		line('Please run this command to finish install QuickWeb:\n' + cmd1 + '\n' + cmd2);
 	}
 }
+
+line(' Thank you for install QuickWeb v0.2.x');
