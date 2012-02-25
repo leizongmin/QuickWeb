cc9f50b82b5e9d23cf56ac1a16f36f15dd078a93
leizongmin <leizongmin@gmail.com>
2012-02-25 20:58:21 +0800

    app_server例子

--- examples/app_server/app/default/config.js
+++ examples/app_server/app/default/config.js
@@ -2,34 +2,112 @@
  * 应用配置
  */
  
+// 注意： 
+// 此配置文件可能会被多次载入，如在程序中有进行其他一些初始化操作，需要自己判断 
+ 
 module.exports = {
-  // 主机名
-  "host":   ["a.ucdok.com", "b.ucdok.com", "localhost", "127.0.0.1"]
   
-  // 虚拟路径
-, "path":   "/"
+  /*
+  主机名，default表示匹配所有主机名。 例如：设置主机名为 ['test.com'], 则仅有访问 test.com 的请求才会转到该应用
+  可设置多个主机名，例如： ['a.com', 'b.com', 'c.com']
+  */
+  'host':   ['default'],
   
-  // request对象配置
-, "request": {
-    'decode get':       true          // 自动解析GET数据
-  , 'decode post':      true          // 自动解析POST数据
-  , 'decode cookie':    true          // 自动解析Cookie数据
-  , 'upload dir':       '/tmp'        // 上传文件目录
-  , 'upload max size':  10485760      // 上传文件最大尺寸
-  }
+  /* 虚拟路径，例如：设置为 /test 时，该应用下的所有路由路径都会加上 /test 前缀 */
+  'path':   '/',
   
-  // response对象配置
-, "response": {
-    'template path':    '.'           // 模板目录
-  , 'home path':        '.'           // 网站根目录
-  , 'http cache age':   31536000      // HTTP静态文件缓存时间，秒
-  , 'enable gzip':      false         // 是否开启gzip压缩输出
-  , 'gzip min size':    256           // 开启gzip压缩输出的最小长度
   
-  , "event": {
-      "header before":  function () {
-        
+  /* ServerRequest对象配置 */
+  'request': {
+    'decode get':       true,          // 自动解析GET数据
+    'decode post':      true,          // 自动解析POST数据
+    'decode cookie':    true,          // 自动解析Cookie数据
+    'upload dir':       '/tmp',        // 上传文件目录
+    'upload max size':  10485760,      // 上传文件最大尺寸（单位：字节），默认为10M
+    
+    // session配置
+    'session type':     'file',        // 使用默认的文件存储引擎
+    'session tag':      'SESSID',      // session标识符
+    'session cookie maxage': 31536000, // Session的Cookie保存时间（单位：秒），默认为1年
+    'session config': {                // 针对该session引擎的配置
+      'path': '/tmp'
+    },
+  
+    /* Request对象事件 */
+    'event': {
+      /*
+      // 解析post成功，在处理post请求时，需要用到POST数据的操作要在此事件之后执行
+      'post complete': function () {
+        console.log(this.post);  // 输出解析的post数据
+      },
+      
+      // 解析post失败
+      'post error': function (err) {
+        console.error(err.stack); // 输出出错信息
+      },
+      
+      // 调用sessionStart()方法后，读取session成功
+      'session start': function (sessionObj) {
+        // sessionObj是用于操作该session的实例
+        // this.session_id是Session ID, this.session是该Session的数据映射
+        console.log(this.session);  // 输出session数据
       }
+      */
+    }
+  },
+  
+  
+  /* ServerResponse对象配置 */
+  'response': {
+    'template path':    'tpl',          // 模板目录，默认为tpl目录
+    'home path':        'html',         // 网站根目录，默认为html目录
+    'http cache age':   31536000,       // HTTP静态文件缓存时间（单位：秒），默认为1年
+    'enable gzip':      false,          // 是否开启gzip压缩输出
+    'gzip min size':    512,            // 开启gzip压缩输出的最小长度（单位：字节），默认为512字节
+    
+    /* 出错页面模板 {{status}}为出错代码   {{message}}为出错信息 */
+    'error page':       '<div style="max-width:500px; margin:auto;">' +
+                        '<h1>{{status}}</h1><pre>{{message}}</pre>' +
+                        '<hr><h3>QuickWeb</h3></div>',
+    
+    /* 文件后缀对应的模板引擎，*为默认的引擎 */
+    'render': {
+      '*':    ['ejs', 'text/html']      // 默认使用ejs引擎
+      /*
+      '后缀名': ['模板引擎', 'Content-Type']
+      */
+    },
+    
+    /* 增加的响应头，每次响应输出都会加上这些响应头 */
+    'header': {
+      /*
+      'X-Powered-By':   'QuickWeb/NSP'
+      */
+    },
+  
+    /* Response对象事件 */
+    'event': {
+      /*
+      // 输出响应头之前
+      'header before': function () {
+        console.log('header before');
+      },
+      
+      // 输出响应头之后
+      'header after': function () {
+        console.log('header after');
+      },
+      
+      // 响应结束之后
+      'end': function () {
+        console.log('输出长度: ' + this._qw_output_length);
+      },
+      
+      // 
+      'send error': function (status, msg) {
+        console.error('出错代码：' + status, '内容：' + msg);
+      }
+      */
     }
   }
 }
--- examples/app_server/config.js
+++ examples/app_server/config.js
@@ -2,44 +2,60 @@
  * 服务器配置
  */
  
+// 注意： 
+// 此配置文件可能会被多次载入，如在程序中有进行其他一些初始化操作，需要自己判断  
+ 
 module.exports = {
 
-  // Worker进程数量
-  "cluster":    1
+  /* Worker进程数量 0表示自动设置为CPU个数 */
+  'cluster':    0,
   
-  // 监听的端口
-, "listen http":  [8080, 81, 82]
+  /* 监听的端口 可同时监听多个端口，例如： [80, 81, 82] */
+  'listen http':  [8080],
   
-  // 消息服务器，可以为Unix Socket
-, "message":      "127.0.0.1:8851"
   
-  // 管理界面
-, "master": {
-    "port":       8850           // 端口
-  , "host":       "127.0.0.1"    // 地址
-  , "admin":      "admin"        // 账户
-  , "password":   "admin"        // 密码
-  }
+  /* 管理界面配置 */
+  'master': {
+    'port':       8850,           // 监听端口
+    'host':       '127.0.0.1',    // 监听地址，设置为0.0.0.0时可以从任何主机访问
+    'admin':      'admin',        // 账户
+    'password':   'admin'         // 密码
+  },
   
-  // 更新状态
-, "status update": {
-    "connector":        60000    // 更新请求统计
-  , "load line":        20000    // 更新系统资源占用
-  , "load line size":   20       // 系统资源占用数据个数
-  , "worker heartbeat": 30000    // Worker进程心跳间隔
-  }
   
-  // 记录的进程异常信息数量
-, "exception log size": 50
+  /* 更新服务器状态 */
+  'status update': {
   
-  // 插件 onExtend 扩展request, response对象之后，执行路由处理程序之前
-, "onExtend": function (req, res) {
+    /* 请求状态统计 */
+    'connector':        10000,    // 更新请求统计周期（单位：毫秒），默认为10秒
+    'connector size':   200,      // 历史请求统计数据个数，默认为200个
+    
+    /* 资源占用统计 */
+    'load line':        10000,    // 更新系统资源占用周期（单位：毫秒），默认为10秒
+    'load line size':   60,       // 系统资源占用数据个数，默认为60个
+  },
+  
+  
+  /* 记录的进程异常信息数量，默认为50个 */
+  'exception log size': 50,
+  
+  
+  /*
+  // onExtend插件， 扩展request, response对象之后，执行路由处理程序之前
+  // 如需要对扩展后的request和response实例进行进一步的处理，可在此处执行
+  'onExtend': function (req, res) {
     console.log(new Date().toUTCString() + ' ' + req.method + ' ' + req.url);
-  }
-
-  // 插件 onRequest 在接收到请求，准备处理之前
-, "onRequest": function (req, res, next) {
+  },
+  */
+  
+  
+  /*
+  // onRequest插件，在接收到请求，准备处理之前，所有的请求都会经过此处进行处理
+  // 中间件可在此处执行  第一个参数为扩展后的request实例，第二个参数为扩展后的response实例
+  // 第三个参数为一个函数，仅当调用此函数时，程序的控制权才会交由QuickWeb Connector来进行路由处理
+  'onRequest': function (req, res, next) {
     res.setHeader('Date', new Date().toUTCString());
     next();
   }
+  */
 }
