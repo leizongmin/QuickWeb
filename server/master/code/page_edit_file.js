61ec542309c2c53bbf5e9d54f9efb35637457b1b
Lei Zongmin <leizongmin@gmail.com>
2012-03-17 17:21:14 +0800

    后台管理中增加了文件管理功能

---
+++ server/master/code/page_edit_file.js
@@ -0,0 +1,71 @@
+/**
+ * 编辑文件
+ */
+ 
+var fs = require('fs'); 
+var path = require('path'); 
+var quickweb = require('../../../');
+var tool = quickweb.import('tool');
+var cluster = quickweb.Cluster;
+ 
+exports.path = '/page/edit_file';
+
+// 显示应用列表
+exports.get = function (req, res) {
+  
+  // 服务器路径
+  var basePath = path.resolve('.');
+  
+  // 路径，出于安全考虑，只运行访问当前服务器路径下的文件
+  var p = path.resolve(basePath, req.get.path);
+  if (p.substr(0, basePath.length) !== basePath) {
+    return res.sendError(403, '没有权限访问该路径！')
+  }
+  
+  // 读取文件内容
+  fs.readFile(p, 'utf8', function (err, data) {
+    if (err)
+      res.sendError(500, err);
+      
+    // 文件类型
+    var extname = path.extname(p);
+    var T = {
+      '.js':    'javascript',
+      '.json':  'json',
+      '.css':   'css',
+      '.html':  'html',
+      '.htm':   'html',
+      '.coffee':'coffee'
+    }
+    var mode = T[extname] || ['javascript', 'JavaScript'];
+    
+    res.renderFile('edit_file.html', {
+      path:   p,
+      data:   data,
+      mode:   mode
+    });
+  });
+  
+}
+
+
+// 保存文件
+exports.post = function (req, res) {
+  req.on('post complete', function () {
+    
+    var filename = req.post.path;
+    var data = req.post.data;
+    
+    fs.writeFile(filename, data, function (err) {
+      if (err)
+        return res.sendError(500, err);
+        
+      req.get.path = filename;
+      return exports.get(req, res);
+    });
+    
+  });
+  req.on('post error', function (err) {
+    res.sendError(500, err);
+  });
+}
---
+++ server/master/code/page_list_dir.js
@@ -0,0 +1,90 @@
+/**
+ * 列出目录
+ */
+ 
+var fs = require('fs'); 
+var path = require('path'); 
+var quickweb = require('../../../');
+var tool = quickweb.import('tool');
+var Parallel = quickweb.import('asynctask').Parallel;
+var cluster = quickweb.Cluster;
+ 
+exports.path = '/page/list_dir';
+
+// 显示应用列表
+exports.get = function (req, res) {
+  
+  // 服务器路径
+  var basePath = path.resolve('.');
+  
+  // 路径，出于安全考虑，只运行访问当前服务器路径下的文件
+  var p = path.resolve(basePath, req.get.path);
+  if (p.substr(0, basePath.length) !== basePath) {
+    return res.sendError(403, '没有权限访问该路径！')
+  }
+  
+  // 列出该目录下的文件
+  fs.readdir(p, function (err, list) {
+    if (err)
+      return res.sendError(500, err);
+      
+    var task = Parallel(function (input, callback) {
+      fs.stat(input, callback);
+    });
+    
+    var V = function (input) {
+      return {key: input, value: path.resolve(p, input)}
+    }
+    var _data = [];
+    for (var i in list)
+      _data.push(V(list[i]));
+    
+    task.start(_data, function (data) {
+     
+      var list = [];
+      for (var i in data) {
+        list.push({
+          name:   data[i].key,
+          path:   path.resolve(p, data[i].key),
+          size:   data[i].value.size,
+          mtime:  data[i].value.mtime,
+          type:   data[i].value.isDirectory() ? 'dir' : 'file'
+        });
+      }
+      
+      res.renderFile('list_dir.html', {
+        list:   list,
+        path:   p
+      });
+    });
+    
+  });
+  
+}
+
+
+
+// 删除文件
+exports.post = function (req, res) {
+  req.on('post complete', function () {
+    
+    var filename = req.post.path;
+    var op = req.post.op;
+    
+    if (op === 'remove') {
+      fs.unlink(filename, function (err) {
+        if (err)
+          res.sendError(500, err);
+          
+        return exports.get(req, res);
+      });
+    }
+    
+    else { 
+      res.sendError(500, '无法识别的指令！');
+    }
+  });
+  req.on('post error', function (err) {
+    res.sendError(500, err);
+  });
+}
